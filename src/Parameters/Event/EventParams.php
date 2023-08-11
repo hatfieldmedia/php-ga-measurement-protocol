31b15fb6b9b5aac87709c577a99fdb06dcff46c2
ccrawfordhatfield <chasecrawford@hatfieldmedia.com>
2023-08-11 13:48:27 -0400

    classroom event params

--- src/Parameters/Event/EventParams.php
+++ src/Parameters/Event/EventParams.php
@@ -12,6 +12,9 @@ use TheIconic\Tracking\GoogleAnalytics\Parameters\CompoundParameter;
 class EventParams extends CompoundParameter
 {
   protected $parameterNameMapper = [
+    '/^grade/' => 'grade',
+    '/^location_id/' => 'location_id',
+    '/^name/' => 'name',
     '/^search_term/' => 'search_term',
     '/^trophy_id/' => 'trophy_id',
     '/^user_id/' => 'user_id',
