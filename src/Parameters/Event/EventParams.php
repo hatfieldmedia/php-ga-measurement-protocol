4d46e320b22b255a2721f78ef7f70c55cb94ab27
ccrawfordhatfield <chasecrawford@hatfieldmedia.com>
2023-08-11 14:26:49 -0400

    add student event params

--- src/Parameters/Event/EventParams.php
+++ src/Parameters/Event/EventParams.php
@@ -12,9 +12,16 @@ use TheIconic\Tracking\GoogleAnalytics\Parameters\CompoundParameter;
 class EventParams extends CompoundParameter
 {
   protected $parameterNameMapper = [
+    '/^classroom_id/' => 'classroom_id',
+    '/^date_of_birth/' => 'date_of_birth',
+    '/^first_name/' => 'first_name',
+    '/^gender/' => 'gender',
     '/^grade/' => 'grade',
     '/^location_id/' => 'location_id',
+    '/^middle_name/' => 'middle_name',
+    '/^last_name/' => 'last_name',
     '/^name/' => 'name',
+    '/^race/' => 'race',
     '/^search_term/' => 'search_term',
     '/^trophy_id/' => 'trophy_id',
     '/^user_id/' => 'user_id',
